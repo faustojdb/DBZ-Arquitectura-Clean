344a1a83bbacd445f47e16c033c18629c0a79419
faustojdb <faustojdb@hotmail.com>
2025-03-23 16:46:08 -0300

    Delete src directory

--- src/components/analysis/components/CreateAnalysisForm/index.tsx
+++
@@ -1,6 +0,0 @@
-import { BasicInfo, Material, AnalysisData } from '../../views/CreateAnalysisView';
-import StepOne from './steps/StepOne';
-import StepTwo from './steps/StepTwo';
-
-export type { BasicInfo, Material, AnalysisData };
-export { StepOne, StepTwo };
--- src/src/components/prices/index.jsx
+++
@@ -1,5 +0,0 @@
-// Archivo index.jsx para exportar todos los componentes
-export { default } from './PriceManagement';
-export { default as PriceManagement } from './PriceManagement';
-export { default as useMaterialsData } from './useMaterialsData';
-export { styles } from './materialsStyles';
